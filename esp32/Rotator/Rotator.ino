7228a70f3ebe030c382377c601d3e5807aa1e970
MatveyHim <98096943+MatveyHim@users.noreply.github.com>
2025-10-25 14:47:04 +0700

    Initial commit

---
+++ esp32/Rotator/Rotator.ino
@@ -0,0 +1,195 @@
+#include <GyverStepper.h>
+#include <Adafruit_NeoPixel.h>
+
+GStepper<STEPPER2WIRE> stepX(320000, 26, 27, 13); // steps/rev, step, dir, en
+GStepper<STEPPER2WIRE> stepY(320000, 32, 33, 26);
+
+#define SerialPort Serial
+#define ledPin  2
+#define numLeds 8
+#define brightness 50
+
+// endstop pin //
+#define Xend 34 // elevation / X axis
+#define Yend 35 // azimuth / Y axis
+
+// offset for endstops //
+#define X_offset 5   //  EL/X
+#define Y_offset -10 //  AZ/Y
+
+#define Cal_on_start false
+
+bool testMode=false;
+bool parking=false;
+bool autoPwr=false;
+
+float az;
+float el;
+String line;
+float azSet;
+float elSet;
+TaskHandle_t Task1;
+
+Adafruit_NeoPixel pix(numLeds, ledPin, NEO_GRB + NEO_KHZ800);
+
+void printAzEl() {
+  SerialPort.print("AZ");
+  SerialPort.print(az, 3);
+  SerialPort.print(" EL");
+  SerialPort.print(el, 3);
+  SerialPort.print("\n");
+}
+
+void cal(){
+  fillStrip(255,0,0); // red, started
+  
+  stepY.setRunMode(KEEP_SPEED);       // find X axis endstop
+  stepY.setSpeedDeg(10);
+  while(digitalRead(Xend)==0 and stepY.getCurrentDeg()<=180) {
+    stepY.tick();
+    if (stepY.getCurrentDeg()>180) while(1){
+      calError();
+    }
+  }
+
+  stepY.setCurrentDeg(90 + X_offset);            // return home
+  stepY.setRunMode(FOLLOW_POS);
+  stepY.setTargetDeg(0, ABSOLUTE);
+  while(stepY.tick() and !testMode) {stepY.tick();}
+  
+  fillStrip(255,70,0); // yellow, half complete
+  
+  stepX.setRunMode(KEEP_SPEED);      // find Y axis endstop
+  stepX.setSpeedDeg(10);
+  while(digitalRead(Yend)==0 and stepX.getCurrentDeg()<=180) {
+    stepX.tick();
+    if (stepX.getCurrentDeg()>180) while(1){
+        calError();
+    }
+  }
+
+  stepX.setCurrentDeg(90 + Y_offset);          // return home
+  stepX.setRunMode(FOLLOW_POS);
+  stepX.setTargetDeg(0,ABSOLUTE);
+  while(stepX.tick() and !testMode) {
+    stepX.tick();
+  }
+
+  fillStrip(0,255,0); // green, completed
+}
+
+void calError(){
+  fillStrip(255,0,0);
+  delay(200);
+  fillStrip(0,0,0);
+  delay(200);
+  vTaskDelay(1);
+}
+
+void fillStrip(int r, int g, int b){
+  for(int i=0; i<numLeds; i++) {
+    pix.setPixelColor(i, pix.Color(r, g, b));
+    pix.show();
+  }
+}
+
+void Task1code( void * pvParameters ){ 
+  while(1){     // Tick on second core
+    stepY.tick();
+    stepX.tick();
+//    yield();
+    vTaskDelay(1);
+//    delayMicroseconds(500);
+  }
+}
+
+
+void setup() {
+  SerialPort.begin(115200);
+  Serial.setTimeout(4);
+  pinMode(Xend, INPUT);
+  pinMode(Yend, INPUT);
+
+  pix.begin();
+  pix.clear();
+  pix.setBrightness(brightness);
+
+  stepY.setAcceleration(9000);
+  stepX.setAcceleration(9000);
+  stepY.setMaxSpeed(8000);    //1500
+  stepX.setMaxSpeed(8000);    //1000
+
+  if (Cal_on_start) cal(); // Calibrate
+  
+  stepY.autoPower(autoPwr);
+  stepX.autoPower(autoPwr);
+
+  delay(500); 
+  
+  xTaskCreatePinnedToCore(Task1code, "Task1", 10000, NULL, 2, NULL,  0);
+}
+
+void loop() {
+  az = stepY.getCurrentDeg();
+  el = stepX.getCurrentDeg();
+  
+  if (Serial.available()) {
+    line = Serial.readString();
+    String param;                                          
+    int firstSpace;                                         
+    int secondSpace;                                        
+    
+    if (line.indexOf("AZ EL")!=-1) {                        
+      printAzEl();                                         
+    } else if (line.startsWith("AZ")) {
+      firstSpace = line.indexOf(' ');
+      secondSpace = line.indexOf(' ', firstSpace + 1);
+      param = line.substring(2, firstSpace);
+      azSet = param.toFloat();
+      param = line.substring(firstSpace + 3, secondSpace);
+      elSet = param.toFloat();
+    } else if (line.indexOf("calibration")!=-1) {
+      cal();
+    }
+  }
+  
+  if (azSet==0.0 and elSet==0.0 and parking){
+    stepY.disable();
+    stepX.disable();
+  }else{
+    stepY.enable();
+    stepX.enable();
+  }
+
+  stepX.setTargetDeg(elSet,ABSOLUTE);
+  stepY.setTargetDeg(azSet,ABSOLUTE);
+
+  
+  while(stepY.tick() or stepX.tick()){
+//    delay(1);
+    stepY.tick();
+    stepX.tick();
+    az=stepY.getCurrentDeg();
+    el=stepX.getCurrentDeg();
+    
+    if (Serial.available()) {
+      line = Serial.readString();
+      String param;                                           //Parameter value
+      int firstSpace;                                         //Position of the first space in the command line
+      int secondSpace;                                        //Position of the second space in the command line
+      
+      if (line.indexOf("AZ EL")!=-1) {                         //Query command received
+        printAzEl();                                          //Send the current Azimuth and Elevation
+      } else if (line.startsWith("AZ")) {                          //Position command received: Parse the line.
+        firstSpace = line.indexOf(' ');                     //Get the position of the first space
+        secondSpace = line.indexOf(' ', firstSpace + 1);    //Get the position of the second space
+        param = line.substring(2, firstSpace);              //Get the first parameter
+        azSet = param.toFloat();                            //Set the azSet value
+        param = line.substring(firstSpace + 3, secondSpace);//Get the second parameter
+        elSet = param.toFloat();                            //Set the elSet value
+      } else if (line.indexOf("calibration")!=-1) {
+        cal();
+      }
+    }
+  }
+}
---
+++ rotatorconn.py
@@ -0,0 +1,222 @@
+import socket
+import sys
+import glob
+import serial
+import time
+from math import asin, sin, cos, atan2, pi, degrees, radians
+from typing import Tuple
+
+XYmode = True
+# XYmode = False
+baudrate = 115200
+# baudrate = 9600
+
+bind_ip = "0.0.0.0"
+bind_port = 4533
+
+AZhome = 0 # AZ / X motor home
+ELhome = 0 # EL / Y motor home
+SendToHome = False
+
+minEL = 0
+minAZ = -90
+maxAZ = 450
+
+az=0
+el=0
+
+global ser
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ser = serial.Serial()
+ser.close()
+
+class Error(Exception):
+    def __str__(self):
+        return 'rotator not connected'
+
+def serial_ports():
+    """ Lists serial port names
+
+        :raises EnvironmentError:
+            On unsupported or unknown platforms
+        :returns:
+            A list of the serial ports available on the system
+    """
+    if sys.platform.startswith('win'):
+        ports = ['COM%s' % (i + 1) for i in range(256)]
+    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+        # this excludes your current terminal "/dev/tty"
+        ports = glob.glob('/dev/tty[A-Za-z]*')
+    elif sys.platform.startswith('darwin'):
+        ports = glob.glob('/dev/tty.*')
+    else:
+        raise EnvironmentError('Unsupported platform')
+
+    result = []
+    for port in ports:
+        try:
+            s = serial.Serial(port)
+            s.close()
+            result.append(port)
+        except (OSError, serial.SerialException):
+            pass
+    return result
+
+def AE2XY(azimuth: float, elevation: float) -> Tuple[float, float]:
+    x = asin(sin(azimuth) * cos(elevation))
+    y = -atan2(cos(azimuth) * cos(elevation), sin(elevation))
+    
+    return (x, y)
+
+def XY2AE(x: float, y: float) -> Tuple[float, float]:
+    x0 = sin(x)
+    y0 = -sin(y)*cos(x)
+    z0 = cos(y)*cos(x)
+    
+    el = asin(z0)
+    az = - atan2(y0, x0)    
+    
+    return pi/2 + az, el
+
+def setPosition(az, el):
+    if XYmode:
+        az = radians(az)
+        el = radians(el)
+        x, y = AE2XY(az, el)
+
+        x = degrees(x)
+        y = degrees(y)
+    else:
+        x=az
+        y=el
+
+    resp = 'AZ'+'{:.3f}'.format(round(x,3))+' EL'+'{:.3f}'.format(round(y,3))+'\n'
+    ser.write(resp.encode('ascii'))
+    # print(resp)
+
+def getPosition():
+    ser.write(b"AZ EL")
+    resp=ser.readline().decode('ascii')
+
+    space=resp.find(' ') # format: AZ XX.xxx EL XX.xxx
+    x=float(resp[2:space])
+    y=float(resp[space+3:len(resp)-1])
+
+    if XYmode:
+        x = radians(x)
+        y = radians(y)
+        az, el = XY2AE(x, y)
+
+        az = degrees(az)
+        el = degrees(el)
+    else:
+        az=x
+        el=y
+
+    if az<0: az=az+360
+    return (az, el)
+
+def parse(text):
+    Ispace=text.find(' ')
+    IIspace=text.find(' ',Ispace+1)
+    az=float(text[Ispace:IIspace])
+    el=float(text[IIspace+1:len(text)-1])
+    return (az, el)
+
+def doComms(client_socket):
+    global az,el
+    try:    
+        request = client_socket.recv(1024)
+    except:
+        print('Client disconnected on recv')
+        request = b''
+
+    text = request.decode('ascii')
+    print ("    Received: "+text[:len(text)-1])
+
+    if text.find('p')!=-1:
+        az, el = getPosition()
+        resp = '{:.3f}'.format(round(az,3))+' '+'{:.3f}'.format(round(el,3))+'\n'
+
+        try:
+            client_socket.sendall(resp.encode('ascii')) 
+        except:
+            print('Client disconnected on send')
+
+        print ("    Sended: "+resp[:len(resp)-1])
+
+    if text.find('P')!=-1:
+        az, el = parse(text) # format: P XX.xxx XX.xxx
+        if az>maxAZ:
+            az = az-360
+            
+        if az<minAZ:
+            az = minAZ
+            
+        if el<minEL:
+            el=minEL
+
+        setPosition(az, el)
+
+        try:
+            client_socket.sendall(b"RPRT 0\n")
+        except:
+            print('Client disconnected')
+
+        print ("    Sended: RPRT 0") 
+    return text
+
+
+ports=serial_ports()
+print(ports)
+
+def search(ports):
+    global ser
+    for port in ports:
+        ser = serial.Serial(port)
+        ser.baudrate = baudrate
+        ser.timeout = 2
+        time.sleep(2)
+        print('trying',port)
+        ser.write(b"AZ EL")
+
+        try:
+            resp=ser.readline().decode('ascii')
+            print('ans:',resp[:len(resp)-1])
+        except:
+            resp=''
+
+        if resp.rfind('AZ')!=-1:
+            print('connected to',port)
+            ser.timeout = 5
+            break
+        ser.close()
+
+search(ports)
+
+while ports==[] or ser.isOpen() == False:
+    print('Connnect rotator!!!')
+    ports=serial_ports()
+    print(ports)
+    search(ports)
+    time.sleep(1)
+    # raise Error
+
+if SendToHome: ser.write(b"AZ"+str(AZhome)+" EL"+str(ELhome))
+
+server.bind((bind_ip, bind_port))
+server.listen()
+print ("Listening on %s:%d" % (bind_ip, bind_port))
+
+try:
+    while True: # работаем постоянно
+        conn, addr = server.accept()
+        print("New connection from " + addr[0])
+        try:
+            while True:
+                text=doComms(conn)
+                if text=="":
+                    break        
+        finally:
+            conn.close()
+finally: server.close()
